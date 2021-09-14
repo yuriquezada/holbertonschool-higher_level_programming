86d4b1f4ac065685fd3f20620d44e66fa41570f1
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-09-13 23:34:55 -0500

    Create 13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * main - check if a singly linked list is a palindrome.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	return (0);
+}
