b2db84d78777fd2e8fb843eca45c1429fbfe4d82
yuriquezada <quezadat.yuri@gmail.com>
2021-09-15 13:04:21 -0700

    Change items for keys and print content

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_items = sorted(a_dictionary.items())
-    for elem in sorted_items:
-        print(elem[0], ':', elem[1])
+    for keys in sorted(a_dictionary.keys()):
+        print('{}: {}'.format(keys, a_dictionary[keys]))
