a1911d35b204aeccbe78f5d43fa205d19bfea4b1
yuriquezada <quezadat.yuri@gmail.com>
2021-09-15 14:57:39 -0700

    Create 10-best_score.py file

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    max_value = max(a_dictionary.values())
+    key = [k for k, v in a_dictionary.items() if v == max_value][0]
+    return key
