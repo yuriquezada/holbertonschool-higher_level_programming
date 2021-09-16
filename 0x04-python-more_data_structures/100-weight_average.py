5b21f7b7828d0e750c9a250e5f1adb1a5054f423
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-09-15 21:09:40 -0500

    Reduce number of lines of weight_average function

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,9 @@
 def weight_average(my_list=[]):
-    if my_list == []:
+    if my_list == [] or my_list is None:
         return 0
     add_numerator = 0
     add_denominator = 0
     for elem in my_list:
-        elem_to_list = list(elem)
-        product = elem[0] * elem[1]
-        add_numerator += product
+        add_numerator += elem[0] * elem[1]
         add_denominator += elem[1]
-    average = add_numerator / add_denominator
-    return average
+    return add_numerator / add_denominator
