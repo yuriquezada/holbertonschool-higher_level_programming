65f012d5310feb5f9c1ed5bc6d62a708d42f6bb7
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-09-15 22:24:04 -0500

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key, value_k in a_dictionary.items():
-        if value_k == value:
+    list_delete = []
+    for key, key_value in a_dictionary.items():
+        if key_value == value:
+            targets.append(key)
             a_dictionary.pop(key, None)
             break
+    for x in list_delete:
+        a_dictionary.pop(x, None)
     return a_dictionary
