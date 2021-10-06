b0627504e17f8632b7f91e230a825820768a9785
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-10-05 23:24:08 -0500

    Delete else condition

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,5 +11,4 @@ class LockedClass:
     def __setattr__(self, attribute, value):
         if attribute == "first_name":
             self.__dict__[attribute] = value
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
+        raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
