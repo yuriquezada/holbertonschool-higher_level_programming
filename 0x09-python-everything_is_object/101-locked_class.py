92ab14fc1ba418c0f6b949669cd5d54a27b11483
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-10-05 23:22:33 -0500

    Add error conditional

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""class LockedClass"""
+"""
+LockedClass
+"""
 
 
 class LockedClass:
-    """A locked class that only lets the user create a instance attribute"""
-     __slots__ = ['first_name']
+    """ No class or object attributes, can't set
+        Except for first_name
+    """
+    def __setattr__(self, attribute, value):
+        if attribute == "first_name":
+            self.__dict__[attribute] = value
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
