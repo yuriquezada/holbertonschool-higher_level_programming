c8bff3ad8519177a196a44c304edf2aeab25811b
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-11-16 18:50:37 -0500

    Create 0-list_databases.sql

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,7 @@
+Enter password: 
+Database                                                                                     
+hbtn_0c_0                                                                                    
+information_schema                                                                           
+mysql                                                                                        
+performance_schema                                                                           
+sys
