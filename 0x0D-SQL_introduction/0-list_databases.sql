6138da5a233551cf20713a1d92cdc8d98f7925a1
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-11-16 18:58:46 -0500

    Update 0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,7 +1,3 @@
-Enter password: 
-Database                                                                                     
-hbtn_0c_0                                                                                    
-information_schema                                                                           
-mysql                                                                                        
-performance_schema                                                                           
-sys
+-- lists all databases of your MySQL server.
+
+SHOW DATABASES;
