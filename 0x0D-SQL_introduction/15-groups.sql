516dbf509e19966a05d0d3b749ce6f3ea14a593f
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-11-16 21:10:37 -0500

    Create 15-groups.sql

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- Script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+SELECT score, COUNT(*) as number FROM second_table
+GROUP BY score
+ORDER BY 2 DESC;
