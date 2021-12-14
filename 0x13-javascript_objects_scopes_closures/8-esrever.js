e5a75d63050884446509516919d748fccb76d433
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-14 13:49:25 -0500

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reduceRight(function (list, current) {
-    list.push(current);
-    return list;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
   }, []);
 };
