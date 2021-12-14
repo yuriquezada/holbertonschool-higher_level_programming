1d2874dcf4cc4b601d9855019a626a52bdf0eded
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-14 13:40:25 -0500

    Export module in one line

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor(size) {
+module.exports = class Square extends Rectangle {
+  constructor (size) {
     super(size, size);
   }
-}
-module.exports = Square;
+};
