70c42760f2c52cb7c8c8aa157473c81e3cb6139f
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-13 23:47:11 -0500

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const element of languages) console.log(element);
+for (const language of languages) console.log(language);
