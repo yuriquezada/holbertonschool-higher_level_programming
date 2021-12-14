e1c0f104d4aed2a07a188f1a909f4867df10a05f
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-14 13:42:15 -0500

    Export module in one line

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-class Square extends require('./5-square') {
-  charPrint(c) {
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
     if (!c) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
-}
-module.exports = Square;
+};
