f6fedb1007453b616b214f4b8c5463af08e6b4fb
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-13 23:49:57 -0500

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
-}
+isNaN(x) ? console.log('Missing number of occurrences') : for (let i = 0; i < x; i++) console.log('C is fun');
