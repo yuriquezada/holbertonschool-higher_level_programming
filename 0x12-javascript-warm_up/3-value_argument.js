42e8975f6468614a28916ba6d4c2cbe398ebb20e
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-13 23:44:25 -0500

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const count = process.argv;
-console.log(!count[2] ? 'No argument' : count[2]);
+const argument = process.argv;
+console.log(!argument[2] ? 'No argument' : argument[2]);
