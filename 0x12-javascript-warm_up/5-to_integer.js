68ff24c315a57da0479bec37093b2cd02bf4ba3b
yuriquezada <quezadat.yuri@gmail.com>
2021-12-14 03:54:32 +0000

    Add text in resut

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const arg = process.argv;
-console.log(!arg[2] ? 'No argument' : isNaN(parseInt(arg[2])) ? 'Not a number' : parseInt(arg[2]));
+console.log(isNaN(parseInt(arg[2])) ? 'Not a number' : 'My number: ' + parseInt(arg[2]));
