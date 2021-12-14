fcc440bab781ee0d0a72535373c89e48e5cba517
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-14 13:31:52 -0500

    Refactor print() method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
   constructor(w, h){
     if (w > 0 && h > 0) [this.width, this.height] = [w, h];
   }
-  print() {
-    for (let i = 0; i < this.height; i++) {
-       let rowX = '';
-       for (let i = 0; i < this.width; i++) rowX += 'x';
-       console.log(rowX);
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
-module.exports = Rectangle;
