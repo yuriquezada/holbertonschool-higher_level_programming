4c718a6b393acac6c2f46f4dabb2349cfa247857
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-14 14:07:00 -0500

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+let dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
   if (newDict[dict[key]] === undefined) {
