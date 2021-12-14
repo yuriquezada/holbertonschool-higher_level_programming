a7c2e17e89f2dfcdb5a06ea608ee01dcb8c26961
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-14 13:30:20 -0500

    Export module in one line

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(w, h) {
+module.exports = class Rectangle {
+  constructor (w, h) {
     if (w > 0 && h > 0) [this.width, this.height] = [w, h];
   }
 };
-module.exports = Rectangle;
