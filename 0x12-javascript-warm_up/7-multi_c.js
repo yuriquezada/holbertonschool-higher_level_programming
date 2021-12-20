aefc19cfe67ec7976b54eb8131f9763377814cd5
yuriquezada <quezadat.yuri@gmail.com>
2021-12-20 09:21:22 -0500

    Add REAME.md

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
+const x = process.argv[2];
 if (isNaN(x)) console.log('Missing number of occurrences');
 for (let i = 0; i < x; i++) console.log('C is fun');
