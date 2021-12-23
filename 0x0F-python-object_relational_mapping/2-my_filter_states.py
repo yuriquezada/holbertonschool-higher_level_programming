91e708a00f12a946ce75e4bd20363a7fa34a222c
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-22 22:07:07 -0500

    Change location of the conditional ___main___.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,10 +5,11 @@
 #                                <mysql password> \
 #                                <database name> \
 #                                <state name searched>
-import sys
-import MySQLdb
-
+'''Connection to database'''
 if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * \
