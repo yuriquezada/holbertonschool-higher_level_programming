d718f9b3bd34ca89ef47580330018624841ae56d
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-22 22:02:34 -0500

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,11 @@
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
-import sys
-import MySQLdb
-
+'''Connection to database'''
 if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
